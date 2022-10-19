b9a16d28f831b03fd884100a498b9498014eab4f
shanmugamdeepak <113435262+shanmugamdeepak@users.noreply.github.com>
2022-10-19 17:48:30 +0530

    Update Expenses.js

--- src/Appcomponents/Expenses/Expenses.js
+++ src/Appcomponents/Expenses/Expenses.js
@@ -17,15 +17,7 @@ function Expenses(props){
         <div className="class">
             <ExpenseFilter title={title}year={year}></ExpenseFilter>
             <ExpenseList filterExpense={filterExpenses}></ExpenseList>
-            {/* {filterExpenses.length===0?(<h3 className="css">Details Not Found</h3>): */}
-            {/* (filterExpenses.map((user2)=>(
-                <ExpenseItem a={user2.name}b={user2.project}c={user2.amount}>
-                    </ExpenseItem>)))} */}
-            {/* <ExpenseItem a={props.date[0].name}b={props.date[0].project}c={props.date[0].amount}></ExpenseItem>
-            <ExpenseItem a={props.date[1].name}b={props.date[1].project}c={props.date[1].amount}></ExpenseItem>
-            <ExpenseItem a={props.date[2].name}b={props.date[2].project}c={props.date[2].amount}></ExpenseItem> */}
             </div>
-            
         </div>
         )
 
